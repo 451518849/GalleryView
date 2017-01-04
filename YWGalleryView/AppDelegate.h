695707c6a2b72e08e1c1525eaafb4abb9e8f5429
JasonWang <451518849@qq.com>
2017-01-04 14:19:49 +0800

    Initial Commit

---
+++ YWGalleryView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YWGalleryView
+//
+//  Created by apple on 2017/1/4.
+//  Copyright © 2017年 wangxiaofa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YWGalleryView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YWGalleryView
+//
+//  Created by apple on 2017/1/4.
+//  Copyright © 2017年 wangxiaofa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
